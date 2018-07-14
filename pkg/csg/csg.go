2f3a5ca82cc21dbf204a7946650c8efcc044afd7
alufers <alufers@wp.pl>
2018-07-14 15:09:34 +0200

    Added procedural sphere

--- pkg/csg/csg.go
+++ pkg/csg/csg.go
@@ -1,5 +1,5 @@
 package csg
 
 func Run() {
-
+	makeSphere(vector3{0, 0, 0}, 2)
 }
---
+++ pkg/csg/procedural_solids.go
@@ -0,0 +1,44 @@
+package csg
+
+import (
+	"math"
+)
+
+func makeSphere(center vector3, radius float64) *csgSolid {
+	const (
+		slices = 16
+		stacks = 8
+	)
+	var polygons []polygon
+	var vertices []vertex
+	makeVertex := func(theta, phi float64) {
+		theta *= math.Pi * 2
+		phi *= math.Pi
+		dir := vector3{
+			math.Cos(theta) * math.Sin(phi),
+			math.Cos(phi),
+			math.Sin(theta) * math.Sin(phi),
+		}
+		vertices = append(vertices, vertex{
+			pos:    dir.times(radius),
+			normal: dir,
+		})
+
+	}
+	for i := 0; i < slices; i++ {
+		for j := 0; j < stacks; j++ {
+			vertices = []vertex{}
+			makeVertex(float64(i)/float64(slices), float64(j)/float64(stacks))
+			if j > 0 {
+				makeVertex(float64(i+1)/float64(slices), float64(j)/float64(stacks))
+			}
+			if j < stacks-1 {
+				makeVertex(float64(i+1)/float64(slices), float64(j+1)/float64(stacks))
+			}
+
+			makeVertex(float64(i)/float64(slices), float64(j+1)/float64(stacks))
+			polygons = append(polygons, newPolygon(vertices, nil))
+		}
+	}
+	return newCsgSolid(polygons)
+}
